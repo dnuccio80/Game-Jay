446cf933704de29a9379d90c5647dad20f245cd2
dnuccio80 <nuccio227@gmail.com>
2023-08-30 13:46:45 -0300

    Modif pre - MultiLanguage

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] private Button playButton;
+
+    [Header("Texts Modificables")]
+    [SerializeField] private TextMeshProUGUI musicVolumeText;
+    [SerializeField] private TextMeshProUGUI soundVolumeText;
+    [SerializeField] private TextMeshProUGUI languageText;
 
     private void OnEnable()
     {
@@ -26,6 +32,23 @@ public class MainMenu : MonoBehaviour
                 LoaderScript.Load(LoaderScript.Scene.Level1Game);
             }
         });
+    }
+
+    public void HandleInputLanguage(int val)
+    {
+
+        if(val == 0)
+        {
+            musicVolumeText.text = "MUSIC VOLUME";
+            soundVolumeText.text = "SOUND VOLUME";
+            languageText.text = "LANGUAGE";
+        }
+        if (val == 1)
+        {
+            musicVolumeText.text = "VOLUMEN DE MUSICA";
+            soundVolumeText.text = "VOLUMEN DE SONIDO";
+            languageText.text = "LENGUAJE";
+        }
     }
 
     public void QuitGame()
