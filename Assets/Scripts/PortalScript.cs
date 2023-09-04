50bdf338cb4239ae856edba9f613395b410c758b
dnuccio80 <nuccio227@gmail.com>
2023-09-04 14:55:48 -0300

    Modificaciones para el elevatorLettersGame

--- Assets/Scripts/PortalScript.cs
+++ Assets/Scripts/PortalScript.cs
@@ -9,15 +9,26 @@ public class PortalScript : MonoBehaviour
     [SerializeField] GameObject portalPartner;
     [SerializeField]  Vector3 offset;
     [SerializeField] private bool canTraslatePlayer;
+    [SerializeField] private bool isElevator;
+    [SerializeField] private bool canDescend;
+    [SerializeField] private Transform descendText;
     private bool catched = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player" && canTraslatePlayer)
         {
-            other.enabled = false;
-            other.transform.position = portalPartner.transform.position + offset;
-            other.enabled = true;
+            if(!isElevator)
+            {
+                other.enabled = false;
+                other.transform.position = portalPartner.transform.position + offset;
+                other.enabled = true;
+            } else if(isElevator)
+            {
+                canDescend = true;
+                descendText.gameObject.SetActive(true);
+            }
+           
         } else if(other.gameObject.tag == "PetInnerCollider")
         {
             other.gameObject.transform.DOScale(Vector3.zero, 0.4f)
@@ -32,6 +43,26 @@ public class PortalScript : MonoBehaviour
         }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player") {
+            if(isElevator)
+            {
+                canDescend = false;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        StarterAssets.StarterAssetsInputs.Instance.OnInteractButtonPressed += StarterAssets_OnInteractButtonPressed; ;
+
+    }
+
+    private void StarterAssets_OnInteractButtonPressed(object sender, System.EventArgs e)
+    {
+        
+    }
 
     private void ScaleToZero()
     {
